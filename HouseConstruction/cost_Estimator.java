2009bd40f6723b1b75d891cbd450c705c2e84a4b
Your Name <govindsrt10@gmail.com>
2019-03-25 11:25:12 +0530

    First Commit

---
+++ HouseConstruction/cost_Estimator.java
@@ -0,0 +1,49 @@
+/**
+
+ * 
+
+ * @author Govind Kumar
+
+ *
+
+ */
+
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+
+class CostEstimatorTest {
+
+
+
+	@Test
+
+	public void TestCostEstimator() {
+
+		CostEstimator ce;
+
+		ce = new CostEstimator('A', 300, false);
+
+		assertEquals(360000.0, ce.findCost());
+
+		ce = new CostEstimator('B', 360.110, false);
+
+		assertEquals(540184.56, ce.findCost());
+
+		ce = new CostEstimator('C', 350, true);
+
+		assertEquals(875000.0, ce.findCost());
+
+		ce = new CostEstimator('C', 400, false);
+
+		assertEquals(720000.0, ce.findCost());
+
+		
+
+	}
+
+
+
+}
